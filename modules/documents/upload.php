670a21803300d4d6528c2c7307e6ba772423f934
Cedrick Embestro <cedrick13wew@gmail.com>
2026-06-30 10:35:24 +0800

    FIx name for upload

--- modules/documents/upload.php
+++ modules/documents/upload.php
@@ -16,23 +16,23 @@ if(isset($_POST['submit']) || isset($_FILES['document']))
 
     $filePath = $uploadDir . $newFileName;
 
-    if(move_uploaded_file($tmpName, $filePath))
-    {
-        $user_id = $_SESSION['user_id'];
+   if(move_uploaded_file($tmpName, $filePath))
+{
+    $user_id = $_SESSION['user_id'];
 
-        mysqli_query($conn,"
-            INSERT INTO documents
-            (file_name, file_path, user_id)
-            VALUES
-            ('$fileName','$newFileName','$user_id')
-        ");
+    mysqli_query($conn,"
+        INSERT INTO documents
+        (file_name, file_path, user_id)
+        VALUES
+        ('$fileName','$newFileName','$user_id')
+    ");
 
-        header("Location: ../../dashboard.php?upload=success");
-        exit();
-    }
-    else
-    {
-        echo "Upload Failed";
-    }
+    header("Location: ../../dashboard.php?upload=success");
+    exit();
+}
+else
+{
+    echo "Upload Failed";
+}
 }
 ?>
